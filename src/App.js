9f1737d370a78a13975d7479e25b4b2b03516b61
asish <asish0sahu84@gmail.com>
2023-03-11 03:11:58 -0800

    news

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+import React,{useState,useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [news, setNews]=useState([]);
+  useEffect(()=>{
+    axios.get("https://newsapi.org/v2/top-headlines?country=in&apiKey=81d6be0bd8d84dce8a9e111136a14eb7")
+    .then((res)=>{
+      console.log(res.data.articles);
+      setNews(res.data.articles);
+    })
+
+  },[])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <div className="row text-center">
+          {
+            news.map((val)=>{
+              return (
+                <div className="col my-3">
+                  <div className="card" style={{width: "18rem"}}>
+                  <img src={val.urlToImage} className="card-img-top" alt="..."/>
+                  <div className="card-body">
+                    <h5 className="card-title">{val.title}</h5>
+                    <p className="card-text">{val.description}</p>
+                    <a href={val.url} class="btn btn-primary">Read More</a>
+                  </div>
+                  </div>
+
+                </div>
+              )
+            })
+          }
+
+        </div>
+      </div>
+     
+    </>
   );
 }
 
